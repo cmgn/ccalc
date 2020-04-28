973ad5e0e889778ee0ec98c40986ec1a38d2937f
cmgn <conor@cmgn.xyz>
2020-04-28 13:06:23 +0100

    Make trace act on a copy of the tree

--- trace.c
+++ trace.c
@@ -1,9 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "ast.h"
 #include "print.h"
 #include "trace.h"
 #include "eval.h"
+
+static struct ast_expr *copy_ast_expr(struct ast_expr *expr)
+{
+	// TODO(cmgn): Just terminating the program when malloc fails would probably
+	// be easier?
+	struct ast_expr *copy = malloc(sizeof(*copy));
+	if (!copy) {
+		goto free_nothing;
+	}
+	*copy = *expr;
+	if (expr->tag == AST_EXPR_CONSTANT) {
+		return copy;
+	}
+	copy->binary_op.left = copy_ast_expr(expr->binary_op.left);
+	if (!copy->binary_op.left) {
+		goto free_copy;
+	}
+	copy->binary_op.right = copy_ast_expr(expr->binary_op.right);
+	if (!copy->binary_op.right) {
+		goto free_copy_left;
+	}
+	return copy;
+
+free_copy_left:
+	free_ast_expr(copy->binary_op.left);
+free_copy:
+	free(copy);
+free_nothing:
+	return 0;
+}
 
 static void internal_trace_ast_expr(struct ast_expr *root, struct ast_expr *expr)
 {
@@ -27,7 +58,13 @@ static void internal_trace_ast_expr(struct ast_expr *root, struct ast_expr *expr
 
 void trace_ast_expr(struct ast_expr *expr)
 {
-	print_ast_expr(expr);
+	struct ast_expr *copy = copy_ast_expr(expr);
+	if (!copy) {
+		printf("could not trace: malloc failed\n");
+		return;
+	}
+	print_ast_expr(copy);
 	printf("\n");
-	internal_trace_ast_expr(expr, expr);
+	internal_trace_ast_expr(copy, copy);
+	free_ast_expr(copy);
 }
